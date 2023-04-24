6610c086dbe17ee0988d7d1a95ec656893e00257
Evan Walter <evan.walter.software@gmail.com>
2023-04-23 19:19:25 -0700

    Failed Binary 1

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -3,10 +3,10 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
 
-    while (lo < hi) {
-        const m = Math.floor(lo + (hi - lo) / 2);
-        const v = haystack[m];
+    let m = lo + (hi - lo) / 2;
+    let v = haystack[m];
 
+    while (lo < hi) {
         if (v === needle) {
             return true;
         } else if (v > needle) {
